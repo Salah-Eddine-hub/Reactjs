aebf79dbeddef35c0603d382fe5ac51f3b9b3254
Salah-eddine Harrachmin <sharrach@e2r5p15.1337.ma>
2023-12-15 13:24:26 +0100

    inline styles in react and his power

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const img = "https://picsum.photos/200"
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "blue";
 
 ReactDOM.render(
-  <div>
-    <h1 className="heading" contentEditable="true" spellCheck="true">My Favourite Foods</h1>
-    <img alt="random" src={img + "?grayscale"}/>
-    <img alt="nuget" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=480:*"/> 
-    <img alt="nuget" src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg"/>
-    <img alt="nuget" src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg"/>
-  </div>,
+  <h1 style={customStyle}>Hello World!</h1>,
   document.getElementById("root")
 );
