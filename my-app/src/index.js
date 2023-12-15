c0bb9f7f58186c56c1a486a2d4dd07ec03e2f8a3
Salah-eddine Harrachmin <sharrach@e2r5p15.1337.ma>
2023-12-15 13:12:45 +0100

    style with react and use attributes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200"
 
-const myName = "Salah-Eddine Harrachmin";
-const cYear = new Date().getFullYear();
 ReactDOM.render(
   <div>
-    <p>Created by {myName}</p>
-    <p>Copyright {cYear}</p>
+    <h1 className="heading" contentEditable="true" spellCheck="true">My Favourite Foods</h1>
+    <img alt="random" src={img + "?grayscale"}/>
+    <img alt="nuget" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=480:*"/> 
+    <img alt="nuget" src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg"/>
+    <img alt="nuget" src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg"/>
   </div>,
-  document.getElementById('root'));
-
-//Create a react app from scratch.
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
+  document.getElementById("root")
+);
