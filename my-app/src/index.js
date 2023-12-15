0a4ade727370e869ca16e9d0641978643ae9139f
Salah-eddine Harrachmin <sharrach@e2r5p15.1337.ma>
2023-12-15 17:31:09 +0100

    how to use app file and let the work more cleaner

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,38 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-
-const newstyle = {
-  color : ""
-};
-
-const cTime = new Date(2022,1,1,19);
-const hours = cTime.getHours();
-let date = "";
-
-if (hours < "12"){
-  date = "moring";
-  newstyle.color = "red";
-}
-else if (hours < "18"){
-  date = "Afternoon";
-  newstyle.color = "green";
-}
-else{
-  date = "evening";
-  newstyle.color = "blue";
-}
-
-ReactDOM.render(<div>
-  <h1 className="heading" style={newstyle}>Good {date}</h1>
-  <p>current time is {hours}</p>
-</div>,
-document.getElementById("root"));
-
-//Create a React app from scratch.
-//Show a single h1 that says "Good morning" if between midnight and 12PM.
-//or "Good Afternoon" if between 12PM and 6PM.
-//or "Good evening" if between 6PM and midnight.
-//Apply the "heading" style in the styles.css
-//Dynamically change the color of the h1 using inline css styles.
-//Morning = red, Afternoon = green, Night = blue.
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
