e19535d64744bbca94b1f3e7e3add114373e4b5f
Salah-eddine Harrachmin <sharrach@e2r4p15.1337.ma>
2023-12-16 14:17:30 +0100

    a practice demonstrate how to work with props easy way

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,28 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import me from "./images/me.png"
-import him from "./images/him.png"
-import she from "./images/she.png"
-import trash from "./images/trash.png"
+import App from "./components/App";
 
-function Contacts(props){
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <img src={props.image} alt="avatar_img" />
-            <p>{props.num}</p>
-            <p>{props.email}</p>
-        </div>
-    );
-}
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-    <Contacts name="me" image={me} num="0662225695" email="myemail@gmail.com" />
-    <Contacts name="him" image={him} num="0662225692" email="hismemail@gmail.com" />
-    <Contacts name="she" image={she} num="0662225693" email="she'semail@gmail.com" />
-    <Contacts name="none" image={trash} num="000000000" email="noneemail@gmail.com" />
-  </div>,
-  document.getElementById("root")
-);
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
