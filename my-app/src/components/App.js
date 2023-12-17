191d49dfb6da8b331ccdc128850b10ca68ec0196
Salah-eddine Harrachmin <sharrach@e2r6p11.1337.ma>
2023-12-17 14:42:12 +0100

    Create a emoji dictionary using map and props <good practice>

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import Card from "./card";
+import emojipedia from "./../imogiepedia"
+import Entry from "./Entry";
 
 
 function App() {
   return (
-    <Card />
+    <div>
+    <h1>
+      <span>emojipedia</span>
+    </h1>
+      {emojipedia.map(Entry)}
+    </div>
   );
 }
 
