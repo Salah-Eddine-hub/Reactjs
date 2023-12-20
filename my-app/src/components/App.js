ff589887dc8194b21a45934f1dc2ef311e9f4269
Salah-eddine Harrachmin <sharrach@e3r9p3.1337.ma>
2023-12-20 18:49:10 +0100

    add state with an example of hover line-through

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import Form from "./Form";
 
-var userIsRegistered = true;
+var isDone = true;
+
+function strike() {
+  isDone = true;
+}
+
+function unStrike() {
+  isDone = false;
+}
 
 function App() {
   return (
-    <div className="container">
-      <Form 
-        isregistred={userIsRegistered}
-      />
-
+    <div>
+      {/* <p style={{textDecoration: "line-through"}}>I am salah ostora</p> */}
+      <p style={isDone ? { textDecoration: "line-through" } : null}>Buy milk</p>
+      <button onClick={strike}>Change to strike through</button>
+      <button onClick={unStrike}>Change back</button>
     </div>
   );
 }
