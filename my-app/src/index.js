f9a1d1133c15656b19b3a27e03ede277ec9843a2
Salah-eddine Harrachmin <sharrach@e3r1p13.1337.ma>
2023-12-24 15:15:31 +0100

    understanding how to destruct things in js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,25 +1,49 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-
-let time = new Date().toLocaleTimeString();
-console.log(time);
+import cars from "./practice"
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+console.log("hi");
 
-//Challenge:
-//1. Given that you can get the current time using:
-//Show the latest time in the <h1> when the Get Time button
-//is pressed.
+const [honda, tesla] = cars;
 
-//2. Given that you can get code to be called every second
-//using the setInterval method.
-//Can you get the time in your <h1> to update every second?
+// const {speedStats:{topSpeed, zeroToSixty}} = tesla;
 
-//e.g. uncomment the code below to see Hey printed every second.
-// function sayHi() {
-//   console.log("Hey");
-// }
-// setInterval(sayHi, 1000);
 
+const hondaTopSpeed = honda.speedStats.topSpeed;
+const hondaTopColour = honda.coloursByPopularity[0];
+const teslaTopSpeed = tesla.speedStats.topSpeed;
+const teslaTopColour = tesla.coloursByPopularity[0];
+
+console.log("honda top speed");
+console.log(hondaTopSpeed);
+console.log(honda);
+console.log(tesla);
+
+console.log(honda.model);
+console.log(tesla.coloursByPopularity[0]);
+
+console.log("this is speed stats");
+console.log(tesla.speedStats.topSpeed);
+
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
