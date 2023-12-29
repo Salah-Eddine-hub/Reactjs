bd5a837ffeb1727f8c88e9686063907e498337aa
Salah-eddine Harrachmin <sharrach@e2r1p2.1337.ma>
2023-12-29 04:09:23 +0100

    what is the difference between call and function component and why better to use hooks

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,39 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import ClassComponent from "./ClassComponent";
+import FunctionalComponent from "./FunctionalComponent";
 
-function App() {
-
-  const [text, setname] = useState("");
-  const [sub, checkIt] = useState("");
-
-  // function fillText(){
-  //   setname();
-  // }
-
-  function handleChange(event){
-    console.log(event.target.value);
-    setname(event.target.value);
-    // event.preventDefault();
+class App extends React.Component {
+  render() {
+    return <ClassComponent />;
   }
-
-  function addText(event){
-    checkIt(text);
-    event.preventDefault();
-    
-  }
-
-  return (
-    <div className="container">
-      <h1>Hello {sub}</h1>
-      <form>
-        <input onChange={handleChange}
-                type="text" 
-                placeholder="What's your name?" 
-                value={text}/>
-        
-      <button onClick={addText}>Submit</button>
-      </form>
-    </div>
-  );
 }
 
 export default App;
