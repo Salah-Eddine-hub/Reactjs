69cc650cabbdab6ae416c9c89bec747d26ec9cdc
Salah-eddine Harrachmin <sharrach@e2r1p2.1337.ma>
2023-12-29 05:23:24 +0100

    handle complex state using one useState and acces each value or name

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,11 +1,38 @@
-import React from "react";
-import ClassComponent from "./ClassComponent";
-import FunctionalComponent from "./FunctionalComponent";
+import React, { useState } from "react";
 
-class App extends React.Component {
-  render() {
-    return <ClassComponent />;
+function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+  });
+
+  function handleChange(event){
+    // console.log(event.target.value);
+    // console.log(event.target.name);
+    const {value, name} = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName"){
+        return {fName: value,
+                lName: prevValue.lName};
+      }
+      else if (name === "lName"){
+        return {fName: prevValue.fName,
+                lName: value};
+      }
+    });
   }
+  
+  return (
+    <div className="container">
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
+      <form>
+        <input name="fName" onChange={handleChange} placeholder="First Name" value={fullName.fName}/>
+        <input name="lName" onChange={handleChange} placeholder="Last Name" value={fullName.lName}/>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
